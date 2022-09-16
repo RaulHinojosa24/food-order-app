52e3b000c6c19eb64e32656a8f58df8776c8e667
RaulHinojosa24 <raul.hinojosa.perez@gmail.com>
2022-09-16 18:00:53 +0200

    Header button and a bit of styling

--- src/components/UI/Header.js
+++ src/components/UI/Header.js
@@ -1,11 +1,11 @@
-import React from "react";
-
 import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = () => {
   return (
     <header className={classes.header}>
-      <h1 className={classes["header__title"]}>Food Order</h1>
+      <h1>Food Order</h1>
+      <HeaderCartButton></HeaderCartButton>
     </header>
   );
 };
---
+++ src/components/UI/HeaderCartButton.js
@@ -0,0 +1,16 @@
+import CartIcon from "../Cart/CartIcon";
+import classes from "./HeaderCartButton.module.css";
+
+const HeaderCartButton = () => {
+  return (
+    <button className={classes["header__button"]}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your cart</span>
+      <span className={classes.badge}>0</span>
+    </button>
+  );
+};
+
+export default HeaderCartButton;
