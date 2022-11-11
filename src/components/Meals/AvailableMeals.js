52f4af71c13305eb553c9e92f8c2f7d733a80f69
RaulHinojosa24 <raul.hinojosa.perez@gmail.com>
2022-11-11 17:30:08 +0100

    fetching data from firebase

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,39 +1,63 @@
+import { useEffect, useState } from "react";
 import Card from "../UI/Card";
 import classes from "./AvailableMeals.module.css";
 import MealItem from "./MealItem/MealItem";
 
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
+// const DUMMY_MEALS = [
+//   {
+//     id: "m1",
+//     name: "Sushi",
+//     description: "Finest fish and veggies",
+//     price: 22.99,
+//   },
+//   {
+//     id: "m2",
+//     name: "Schnitzel",
+//     description: "A german specialty!",
+//     price: 16.5,
+//   },
+//   {
+//     id: "m3",
+//     name: "Barbecue Burger",
+//     description: "American, raw, meaty",
+//     price: 12.99,
+//   },
+//   {
+//     id: "m4",
+//     name: "Green Bowl",
+//     description: "Healthy...and green...",
+//     price: 18.99,
+//   },
+// ];
 
 const AvailableMeals = () => {
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(
+      "https://react-http-b3296-default-rtdb.europe-west1.firebasedatabase.app/meals.json"
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setMeals(
+          Object.keys(data).map((key) => {
+            return { id: key, ...data[key] };
+          })
+        );
+
+        setIsLoading(false);
+      });
+  }, []);
+
+  if (isLoading) {
+    return <section className={classes["meals-loading"]}>Loading...</section>;
+  }
+
   return (
     <Card className={classes["available-meals"]}>
       <ul>
-        {DUMMY_MEALS.map((meal) => (
+        {meals.map((meal) => (
           <MealItem meal={meal} key={meal.id}></MealItem>
         ))}
       </ul>
